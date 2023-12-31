8e9324c08537306402428ebcf9ca5823b3c35264
George Ma <george@local>
2023-12-31 13:40:01 +1100

    move file to the right place

--- server/protocol.go
+++ server/protocol.go
@@ -6,53 +6,34 @@ import (
 	"github.com/z-george-ma/buggy/v2/tcp"
 )
 
-type HttpTunnelConn struct {
-	tcp.Conn
-	Request tcp.HttpRequest
-}
-
-var proxyResponse []byte = []byte("HTTP/1.1 200 OK\r\n\r\n")
-
-func HttpTunnelAccept(conn tcp.Conn) (ret *HttpTunnelConn, err error) {
-
-	ret = &HttpTunnelConn{
-		Conn: conn,
-	}
-
-	ret.Request, err = tcp.ParseHttpRequest(conn)
-	if err != nil {
-		return
-	}
-
-	if ret.Request.Method != "CONNECT" {
-		err = fmt.Errorf("Method %s not supported", ret.Request.Method)
-		return
-	}
-
-	_, err = conn.Write(proxyResponse)
-	if err != nil {
-		return
-	}
-
-	return ret, conn.Flush()
-}
+var connectResponse []byte = []byte("HTTP/1.1 200 OK\r\n\r\n")
 
 func HandleConnection(dialer *tcp.TcpDialer, conn *tcp.TlsConn) (err error) {
 	if err = conn.Conn.Handshake(); err != nil {
 		return
 	}
 
-	httpTunnel, err := HttpTunnelAccept(conn)
-	if err != nil {
+	request, err := tcp.ParseHttpRequest(conn)
+
+	if request.Method != "CONNECT" {
+		err = fmt.Errorf("Method %s not supported", request.Method)
 		return
 	}
 
-	down, err := dialer.Dial(httpTunnel.Request.Url)
+	if _, err = conn.Write(connectResponse); err != nil {
+		return
+	}
+
+	if err = conn.Flush(); err != nil {
+		return
+	}
+
+	down, err := dialer.Dial(request.Url)
 	if err != nil {
 		return
 	}
 
 	defer down.Close()
 
-	return tcp.Splice(httpTunnel, down)
+	return tcp.Splice(conn, down)
 }
